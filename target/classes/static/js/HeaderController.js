8d6ccd0d60a43168dd8cdee27c98f1a20d39b792
tushar20993 <tushar20993@gmail.com>
2018-03-30 17:55:41 +0530

    removed target class

--- target/classes/static/js/HeaderController.js
+++
@@ -1,6 +0,0 @@
-portal.controller("HeaderController", function($scope, $location){
-	
-	$scope.isActive = function(viewLocation){
-		return (viewLocation == $location.path()) ? "active" : "";
-	};
-})
