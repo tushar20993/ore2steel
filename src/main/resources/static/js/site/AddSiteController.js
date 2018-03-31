1478918dac9b11b8adc1f105c033d0ed5740c106
tushar20993 <tushar20993@gmail.com>
2018-03-31 18:29:13 +0530

    added db dump

--- src/main/resources/static/js/site/AddSiteController.js
+++ src/main/resources/static/js/site/AddSiteController.js
@@ -1,6 +1,6 @@
 portal.controller("AddSiteController", function($scope, $rootScope, $http, $uibModalInstance, sites, Notification, GlobalSpinner){
 	$scope.site = {};
-	$scope.sites = sites;
+	$scope.oldSites = [];
 	$scope.statuses = [];
 	
 	$http.get("/company/getAll").then(
@@ -10,11 +10,37 @@ portal.controller("AddSiteController", function($scope, $rootScope, $http, $uibM
 				Notification.error("Failed to fetch companies");
 			});
 	
+	
+	
 	$scope.stateCodes =  JSON.parse(JSON.stringify($rootScope.stateCodes));
 	$scope.statuses = JSON.parse(JSON.stringify($rootScope.statuses));	
 	$scope.close = function(){
 		$uibModalInstance.dismiss('cancel');
 	};
+	
+	$scope.onCompanySelect = function(company){
+		GlobalSpinner.show();
+		var company = $scope.site.siteId.company;
+		if(company == undefined){
+			$scope.oldSites = [];
+			GlobalSpinner.hide();
+			return;
+		}
+		$http.get("/site/get?id=" + company.companyId).then(
+				function success(response){
+					$scope.oldSites = response.data;
+					GlobalSpinner.hide();				
+				},
+				function fail(response){
+					GlobalSpinner.hide();
+					Notification.error("Error in getting sites for " + company.companyName);
+				});
+	};
+	
+	$scope.onSiteTypeaheadSelect = function(item, model, label){
+		Notification.error(label + " already exists!");
+		$scope.site.siteName = "";
+	}
 	
 	$scope.saveSite = function(){
 		GlobalSpinner.show();
